1d7a93e1a3e9a4f7dd4584befe28753aec1143a6
Janis Shin <49432660+janisshin@users.noreply.github.com>
2022-11-11 22:42:59 -0800

    edited logic into bash

--- utils.sh
+++ utils.sh
@@ -5,17 +5,17 @@ chooseWordbank(){
   echo "1) All possible 5-letter words\n2) Only the words from the official answer list\nChoose either 1 or 2 and press enter."
   read mode
 
-  while $mode != 1 or 2:
+  while [ "$mode" != "1" ] || [ $x != "2" ]
   do
       echo "Choose either 1 or 2 and press enter."
       read mode
   done
 
-  if $mode == 1:
-  do
-      curl -s https://raw.githubusercontent.com/janisshin/wordle-list-generator/main/words | grep ...
-  elif $mode ==2:
-  do
+    if [[ "$mode" == "1" ]]
+    then
+        curl -s https://raw.githubusercontent.com/janisshin/wordle-list-generator/main/words | grep ...
+  elif [[ "$mode" == "2" ]]
+  then
       curl -s https://raw.githubusercontent.com/janisshin/wordle-list-generator/main/words | grep ...
   done
 }
@@ -24,20 +24,23 @@ takeInLetters(){
     echo "Enter all guessed letters in order: "
     read guesses
     
-    while ${#guesses}! 5:
+    while [[ ${#guesses} != 5 ]]
     do
         echo "The number of letters entered must be 5. Please try again."
         read guesses
     done
    
 
-    echo "Enter in the color codes of your guessed letters in order:\n B:black\nY:yellow\nG:green"
-    echo "For example, black, yellow, black, green, black would be BYBGB"
+    echo "Enter in the color codes of your guessed letters in order:\n b:black\ny:yellow\ng:green"
+    echo "For example, black, yellow, black, green, black would be bybgb"
     read color_codes
     
-    while ${#color_codes}! 5 OR ${color_codes} contains non BYG:
+    # make color_code lowercase
+    color_codes=`echo "$color_codes" | tr '[:upper:]' '[:lower:]'`
+
+    while [[ ${#color_codes} != 5 ]] || [[ $color_codes != +("b" "y" "g") ]]
     do
-        echo "The number of letters entered must be 5 and only "B", "Y", and "G" are accepted as color codes. Please try again."
+        echo "The number of letters entered must be 5 and only "b", "y", and "g" are accepted as color codes. Please try again."
         read $color_codes
     done
 
@@ -77,5 +80,4 @@ program(){
         checkToContinue()
     done
 }
-
 
