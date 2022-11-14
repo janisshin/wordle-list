9321e2c16d0a4a9548ece5dceba1d9b07d43afbf
Janis Shin <49432660+janisshin@users.noreply.github.com>
2022-11-13 22:50:46 -0800

    progress on regex

--- utils.sh
+++ utils.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+# yes, I know that NYT highlights incorrect letters gray, but since both "gray" and "green" start with "gr", I will just pretend gray = black
+
+
 chooseWordbank(){
   echo "which wordbank do you want to use?"
   echo "1) All possible 5-letter words\n2) Only the words from the official answer list\nChoose either 1 or 2 and press enter."
@@ -13,10 +16,10 @@ chooseWordbank(){
 
     if [[ "$mode" == "1" ]]
     then
-        curl -s https://raw.githubusercontent.com/janisshin/wordle-list-generator/main/words | grep ...
+        wordFile="https://github.com/janisshin/wordle-list-generator/blob/main/all_possible.txt.txt"
   elif [[ "$mode" == "2" ]]
   then
-      curl -s https://raw.githubusercontent.com/janisshin/wordle-list-generator/main/words | grep ...
+      wordFile="https://github.com/janisshin/wordle-list-generator/blob/main/only_solutions.txt"
   done
 }
 
@@ -47,8 +50,73 @@ takeInLetters(){
     # somehow store these values and positions   
 }
 
-performRegex(){
+positionalSearch(varName, letter, position, invert){
+    # varName = string (this is because bash does not allow returns in functions)
+    # letter = char
+    # position = array
+    # invert = bool
+
+    blank="....."
+    if [[ $invert ]]; then
+        for letter, position in gList
+        do
+            declare ${varName}=`echo $positions | sed s/./$letter/$position`
+        done
+    else
+        for letter, position in gList
+        do
+            declare ${varName}=`echo $positions | sed s/./$letter/$position`
+        done
+    fi
+    
+}
+
+performRegex(bList, yList, gList){
     #construct the string
+    # bList is a list of letters that are highlighted black
+    # yList is an array where each element consists one letter and a variable amount of numbers
+    # gList is an array where each element consists of one letter and one number
+
+    elimBlack=`curl -s "$wordFile" | grep -v $bList`
+
+    # green
+    positionalSearch(gString, (${gList[0]}), (${gList[1]}), false)
+
+    # yellow
+    positionalSearch(yString, (${gList[0]}), (${gList[1]}), false)
+    ySearch=""
+    yExclude=""
+    for yArray in yList
+    do
+        # get first element from array
+        yLetter=(${yArray[0]})
+        ySearch+="/$yLetter/"
+        yPositions=(${yArray[@]:1})
+
+        for place in yPositions
+        do
+            yString+=`echo $positions | sed s/./$yLetter/$place`
+        done
+        
+    done
+    yExclude+=" | grep -v $yString"
+    
+    # replace all // with / && /
+    ySearch=echo "'${yString//\/\//\/ && \/}'"
+    # surround with single quotes
+    # ySearch="'$ySearch'" # DELETE?
+
+    
+
+
+
+
+    filtered=`echo $elimBlack | awk $gString | awk $yString`
+    
+    
+
+    filtered=`echo $filtered |       `
+
 }
 
 provideWords(){
@@ -80,4 +148,3 @@ program(){
         checkToContinue()
     done
 }
-
